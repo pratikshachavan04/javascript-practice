43b3aa2123859cb9977adaca75851f0192664c75
Pratiksha Chavan <167400021+pratikshachavan04@users.noreply.github.com>
2024-06-20 14:19:55 +0000

    conversion of data types

---
+++ 01_basic/03_conversionsOperation.js
@@ -0,0 +1,27 @@
+let age = undefined
+
+
+//console.log(typeof age);
+
+let valueInNumber = Number(age)
+// console.log(typeof valueInNumber);
+// console.log (valueInNumber);
+
+// "11"=> 11
+// "11ab" => NAN
+// true => 1 ; false >= 0;
+
+
+let isLoggedIn = "p"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false 
+// "p" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
