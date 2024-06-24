333a10b313cee69dcd9653d5d76b9ee7101919f7
Pratiksha Chavan <167400021+pratikshachavan04@users.noreply.github.com>
2024-06-24 11:45:41 +0000

    datatypes in js

---
+++ 01_basic/datatypes.js
@@ -0,0 +1,37 @@
+//primitive 
+
+// 7 types : string , Number, Boolean , null , undefined , symbol ,Big int
+ 
+const score = 100
+const scoreValue = 100.2
+
+const isLoggedIn = false
+const outsideTemp = null
+let useEmail;
+
+const id = Symbol("123")
+const anotherId = Symbol("123")
+
+//console.log (id === anotherId);
+
+// const bigNumber  = 4545454545454545n
+
+
+
+//Reference (Non Ptimitive)
+
+// Array, Objects, Functions
+
+const heros = ["Hatim","Vikraal"]; // array
+
+letmyObj = {                      //object
+    name: "pratiksha",
+    age: 26,
+}
+
+
+const myFunction =function(){
+    //console.log("Hello world");
+}
+
+//console.log (typeof heros);
