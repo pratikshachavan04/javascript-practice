0bb514110e02544b04c2f5bfb64321c3a600a5f3
Pratiksha Chavan <167400021+pratikshachavan04@users.noreply.github.com>
2024-06-24 07:55:44 +0000

    comparision and data types

--- 01_basic/03_conversionsOperation.js
+++ 01_basic/03_conversionsOperation.js
@@ -21,7 +21,37 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 // "" => false 
 // "p" => true
 
-let someNumber = 33
+let someNumber = 11
 
 let stringNumber = String(someNumber)
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+
+//**********************************  Operations ************************************
+let value = 1
+let negValue = -value
+// console.log(negValue);
+
+//console.log(2+2);
+//console.log(2-2);
+// console.log(2*2);
+// console.log(2/5);
+// console.log(2%5);
+
+let str1 = "Hi"
+let str2 =" pratiksha"
+let str3 = str1 + str2
+//console.log(str3);
+
+//console.log ("1"+ 2);
+//console.log (1 +"2");
+//console.log ("1"+2+3);
+//console.log (1+2+"3");
+
+//console.log( (3+4) * 5 % 2);  //use paranthesis for this type of value
+
+let gameCounter = 100
+++gameCounter;
+//console.log(value);
+
---
+++ 01_basic/04_comparison.js
@@ -0,0 +1,16 @@
+//console.log("2" > 1);
+//console.log ("02" > 1);
+
+//console.log(null > 0);
+//console.log(null == 0);
+//console.log(null >= 0);
+
+
+//comparision and equality check is diff in js
+// (=== is strict check in js  eg. it compared value & data types )
+
+
+//console.log(undefined == 0);
+//console.log(undefined > 0);
+//console.log(undefined < 0);
+
