bc6da7aa70d1e1da605975dbc9e78ab64ec2b8ab
Pratiksha Chavan <167400021+pratikshachavan04@users.noreply.github.com>
2024-07-10 09:28:46 +0000

    scopes

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,65 @@
+//var c = 1
+
+
+let a = 202
+
+
+if (true) {
+
+    let a = 22
+    const b = 11
+    // console.log("INNER: ", a);
+  
+}
+
+//console.log(a);
+
+//console.log(b);
+
+
+
+function one (){
+    const username = "pc"
+    
+    function two(){
+        const acct = "insta"
+       // console.log(username);
+        
+    }
+    //console.log(acct);
+
+
+    two()
+}
+
+one()
+
+
+
+if (true){
+    const username = "pc"
+    if(username ==="pc"){
+        const website = " youtube"
+        //console.log(username + website);
+ 
+    }
+   // console.log(website);  
+}
+//console.log(username);
+
+
+
+//------------------------------------------interesting------------------------------------------------- */
+
+
+function addone(num){
+    return num + 1
+
+}
+addone(5)
+
+const addtwo = function(num){
+    return num + 2
+}
+
+addtwo(10)
