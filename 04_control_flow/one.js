0215536b5f338a3096cd0f6ca452b9ed1f5f321e
Pratiksha Chavan <167400021+pratikshachavan04@users.noreply.github.com>
2024-07-15 10:13:06 +0000

    if else understanding

---
+++ 04_control_flow/one.js
@@ -0,0 +1,23 @@
+// if else 
+
+// const rainyDay = false
+
+// if (rainyDay){
+//     console.log("step out")
+// }
+// else{
+//     console.log("stay at home")
+// }
+
+// console.log("yes");
+
+
+const marks = 100
+
+if (marks > 100) {
+    console.log("A+")
+
+} else if (marks > 90){
+    console.log("A");
+}
+else (marks >)
