6b9477d73aa928f66568641c7efd9b550b5ed258
Pratiksha Chavan <167400021+pratikshachavan04@users.noreply.github.com>
2024-07-15 11:16:03 +0000

    control flow code files

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -11,13 +11,40 @@
 
 // console.log("yes");
 
+// const temperature = 40
 
-const marks = 100
+// if (temperature === 455) {
+//     console.log("less than 50");
+// }else {
+//     console.log("greater than 50");
+// }
+// console.log ("executed");
 
-if (marks > 100) {
-    console.log("A+")
+// SHORT HAND NOTATION
+// const balance = 1000   //this type of code is implicit function ..its not a good practice of code 
+// if (balance > 500) console.log("less than 1000"), console.log("test1");
 
-} else if (marks > 90){
-    console.log("A");
+// const balance = 1000
+
+// if (balance < 500) {
+//     console.log("less than 500");
+// } else if (balance < 750){
+//     console.log("less than 750");
+// } else if (balance < 900){
+//     console.log("less than 900")
+// } else {
+//     console.log("less than 1100");
+// }
+
+const userLoggedIn = true 
+const debit = true
+const loggedInFromGoogle = true  
+const  loggedInFromGmail = false 
+const guestUser = false 
+if (userLoggedIn && debit){
+    console.log("allowed");
 }
-else (marks >)
+
+if (loggedInFromGoogle || loggedInFromGmail || guestUser ){
+    console.log("allowed");
+}
