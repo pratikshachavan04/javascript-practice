c0969ed49db0add5d3634f55dba335a428615337
Pratiksha Chavan <167400021+pratikshachavan04@users.noreply.github.com>
2024-07-17 13:41:07 +0000

    for off

---
+++ 05_iteration/for_off.js
@@ -0,0 +1,16 @@
+// for of 
+
+// [" ", " ", " "]
+// [{}, {}, {}]
+
+//  const arr = [ 1,2,3,4,5]
+
+//  for (const val of arr) {
+//     console.log(val);
+//  }
+
+ const greeting = "hello pc"
+for (const greet of greeting ) {
+   console.log(`each char is ${greet}`);
+   
+}
