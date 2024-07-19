60a6d3253325c6a0249d2973d99a6d88bdb06c5f
Pratiksha Chavan <167400021+pratikshachavan04@users.noreply.github.com>
2024-07-19 12:53:07 +0000

    High Order Array

--- 05_iteration/for_off.js
+++ 05_iteration/for_off.js
@@ -9,8 +9,40 @@
 //     console.log(val);
 //  }
 
- const greeting = "hello pc"
-for (const greet of greeting ) {
-   console.log(`each char is ${greet}`);
-   
+//  const greeting = "hello pc"
+// for (const greet of greeting ) {
+
+//    console.log(`each char is ${greet}`);
+// }
+
+// const greetings = "welcome"
+// for (const greett of greetings){
+//    console.log(`very warm ${greett}`);
+// }
+
+
+
+// Maps 
+
+const map  = new Map()
+map.set('IN', "India")
+map.set('USA', "United Stated of  America")
+map.set('UK', "United Kingdom ")
+// console.log(map);                          // for off loop 
+
+
+for (const [key,value] of map) {
+   console.log(key, ':-', value);
 }
+
+
+const myObject = {
+   'froot1': 'apple',
+   'froot2': 'cherry'
+}
+
+
+// for off loop with object doesnt work 
+// for (const [key, value] of myObject) {
+//    console.log(key, ':-', value);
+// }
