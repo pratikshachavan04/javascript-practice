87ac70fa50f9359f2e8f5023e9d58b505766875e
Pratiksha Chavan <167400021+pratikshachavan04@users.noreply.github.com>
2024-07-19 13:54:48 +0000

    for of loop

---
+++ 05_iteration/for of.js
@@ -0,0 +1,53 @@
+// for of 
+
+// [" ", " ", " "]
+// [{}, {}, {}]
+
+//  const arr = [ 1,2,3,4,5]
+
+//  for (const val of arr) {
+//     console.log(val);
+//  }
+
+
+//  const greeting = "hello pc"
+// for (const greet of greeting ) {
+
+//    console.log(`each char is ${greet}`);
+// }
+
+// const greetings = "welcome"
+// for (const greett of greetings){
+//    console.log(`very warm ${greett}`);
+// }
+
+
+
+// Maps 
+
+const map  = new Map()
+map.set('IN', "India")
+map.set('USA', "United Stated of  America")
+map.set('UK', "United Kingdom ")
+// console.log(map);                          // for off loop 
+
+
+for (const [key,value] of map) {
+   console.log(key, ':-', value);
+}
+
+
+const myObject = {
+   'froot1': 'apple',
+   'froot2': 'cherry'
+}
+
+
+
+
+
+
+// for off loop with object doesnt work 
+// for (const [key, value] of myObject) {
+//    console.log(key, ':-', value);
+// }
